38bb4c23839fb174243a0a3acdddc934c56e7d63
wtv_viz_a <wtv_viz_a_svc@asv.local>
2024-06-18 13:25:54 +0200

    php

--- index.php
+++ index.php
@@ -18,12 +18,27 @@ $hostnames = [
 
 $results = [];
 
+function fetch_data($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 1); // 1 second timeout
+    $response = curl_exec($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_code != 200) {
+        return false;
+    }
+    return $response;
+}
+
 foreach ($hostnames as $hostname) {
     $renderer_url = "http://$hostname:61000/api/v1/renderer/layer";
     $version_url = "http://$hostname:61000/api/v1/system/version";
 
-    $renderer_response = @file_get_contents($renderer_url);
-    $version_response = @file_get_contents($version_url);
+    $renderer_response = fetch_data($renderer_url);
+    $version_response = fetch_data($version_url);
 
     if ($renderer_response === FALSE || $version_response === FALSE) {
         $results[] = [
@@ -37,7 +52,7 @@ foreach ($hostnames as $hostname) {
 
     $renderer_data = json_decode($renderer_response, true);
     $version_data = json_decode($version_response, true);
-    
+
     $artist_free = true;
     foreach ($renderer_data as $item) {
         if ($item['Scene'] !== "00000000-0000-0000-0000-000000000000") {
